0fa58ff498b0b0cbeb44a95e5b810e52704a766c
Jazzamat <omergul2001@gmail.com>
2024-07-30 11:25:47 +1000

    added loop around the tcp stream

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,13 @@
 use std::{fs, io::{self, stdin, stdout, Read, Write}, net::TcpStream, str::FromStr};
 
+static SERVER_ADDRESS: &str = "0.0.0.0:8080";
+
 fn main() -> io::Result<()>{
     renderAsciiTitle();
     let username = intro();
     cli(username);
     Ok(())
 }
-
 
 fn intro() -> String {
     println!("Welcome to jazaChat");
@@ -19,21 +20,16 @@ fn intro() -> String {
     String::from_str(username).unwrap()
 }
 
-
 fn renderAsciiTitle() {
-
-
     let contents = fs::read_to_string("ascititle.txt").expect("could read find this file");
     println!("{contents}")
-
-
-
 }
-
 
 fn cli(username: String) {
     let stdin = stdin();
     let mut chat = String::new();
+
+    let mut stream = TcpStream::connect(SERVER_ADDRESS).unwrap();
     loop {
         // get input from stdin
         let mut buf = String::new();
@@ -46,19 +42,26 @@ fn cli(username: String) {
         match command {
             _ if command == "$get\n"  => {
                 stdout().flush().unwrap();
-                get_chat();
+                // get_chat(&mut stream);
             }
             _ => {
                 // decorate and send message to server
                 let buf = add_username(buf, &username);
                 let buf = buf.as_str();
-                let mut stream = TcpStream::connect("3.25.92.165:8080").unwrap();
-                stream.write(buf.as_bytes()).unwrap();
+
+                match stream.write(buf.as_bytes()) {
+                    Ok(response) => {}
+                    Err(error) => {
+                        println!("jazaChat: Error occured when writing:");
+                        println!("{error:?}");
+                    }
+                }
+                receive_from_stream(&mut stream);
             }
         }
+        stream.flush().unwrap();
     }
 }
-
 
 fn add_username(buf: String, username: &String) -> String { 
     let mut result = String::new();
@@ -70,14 +73,16 @@ fn add_username(buf: String, username: &String) -> String {
     result
 }
 
-fn get_chat() -> String {
-
+fn get_chat(stream: &mut TcpStream) -> &mut TcpStream {
     // send over the $get command.
     let mut get_command = String::new();
     get_command.push_str("$get\n");
-    let mut stream = TcpStream::connect("3.25.92.165:8080").unwrap();
     stream.write(get_command.as_bytes()).unwrap();
+    receive_from_stream(stream);
+    stream
+}
 
+fn receive_from_stream(stream: &mut TcpStream) {
     // reveice the chat from server
     let mut received = [0;10000];
     stream.read(&mut received).unwrap();
@@ -87,13 +92,11 @@ fn get_chat() -> String {
     let value = String::from_str(received).unwrap();
     render(received);
     stdout().flush().unwrap();
-    value
 }
 
-fn render(chat: &str) {
+
+fn render(chat: &str) { // problably don't need this
     for c in chat.chars() {
         print!("{}", c)
     }
 }
-
-
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,76 @@
-use std::{io::{self, Read, Write}, net::TcpListener};
+use std::{
+    io::{self, stdout, Read, Write},
+    net::{TcpListener, TcpStream}, thread,
+};
 
 
-fn main () -> io::Result<()> {
+fn main() -> io::Result<()> {
     server()
 }
 
-fn server() -> io::Result<()>{
-
+fn server() -> io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     println!("This is the jazaServer. Listening on port 8080");
-
     let mut chat = String::new();
+    let mut streams: Vec<TcpStream> = Vec::new();
 
     for stream in listener.incoming() {
-        let mut buf = [0;10];
         let mut stream = stream.unwrap();
-        stream.read(&mut buf)?;
-        let msg = String::from_utf8_lossy(&buf);
-        let msg = msg.into_owned();
-
-        chat.push_str(msg.as_str());
-
-
-        println!("Chat: {chat:?}");
-        let mut buf = chat.as_bytes();
-        stream.write(&mut buf)?;
+        streams.push(stream.try_clone().unwrap());
+        //thread::spawn( || {println!("sf");
+            handle_stream(&mut stream, &mut chat, &mut streams)
+        //});
     }
     Ok(())
+}
+
+fn handle_stream(stream: &mut TcpStream, chat: &mut String, streams: &mut Vec<TcpStream>) {
+    println!("New stream lets go!: {stream:?}");
+    loop {
+
+        // need to figure out if the stream is no longer active so I can avoid an infinite loop
+
+        let mut buf = [0; 100];
+        stream.read(&mut buf).unwrap();
+        println!("{buf:?}");
+        if buf[0] == 0 {
+            println!("Connection closed");
+            streams.retain(|x| x.peer_addr().unwrap() != stream.peer_addr().unwrap());
+            break;
+        }
+
+        let msg = String::from_utf8_lossy(&buf);
+        let command = msg.clone().into_owned();
+        let command = command.as_str().trim_matches(char::from(0));
+
+        match command {
+            _ if command == "$get\n" => {
+                stdout().flush().unwrap();
+                let mut buf = chat.as_bytes();
+                stream.write(&mut buf).unwrap();
+            }
+            _ => {
+                let msg = msg.into_owned();
+                let msg = msg.trim_matches(char::from(0));
+                chat.push_str(msg);
+                println!("Chat: {chat:?}");
+                println!("notifying streams...");
+                notify_streams(streams, msg);
+            }
+        }
+        println!("IM ABOUT TO FLUSH");
+        stream.flush().unwrap();
+        println!("I flushed...");
+    }
+}
+
+fn notify_streams(streams: &mut Vec<TcpStream>, message: &str) {
+    println!("For now imaging that this worked");
+    for stream in streams {
+        println!("Stream: {stream:?}");
+        stream.write(message.as_bytes()).unwrap();
+    }
+}
+
+fn write_to_clients() {
 }
